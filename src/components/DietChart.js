a06663b7e81d0c6e0ea90c65d43c7205b89d5363
parmod <parmodmagotra6391@gmail.com>
2023-08-14 20:21:16 +0530

    implemented working of bmi calculator

---
+++ src/components/DietChart.js
@@ -0,0 +1,14 @@
+const DietChart = () =>{
+    return (
+        <div>
+            <div>
+                {/*  FIRST CHART FROM BMI LESSER THAN 18.5 */}
+                <div></div>
+                 {/*  SECOND CHART FROM BMI LESSER THAN 18.5 */}
+                <div></div>
+                 {/*  THIRD CHART FROM BMI LESSER THAN 18.5 */}
+                <div></div>
+            </div>
+        </div>
+    )
+}
